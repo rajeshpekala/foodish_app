2ca4431b1920836656d8f87d3f5771611355a1ea
rajeshpekala <pekalarajesh001@gmail.com>
2023-12-29 17:51:10 +0530

    changed_api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,4 +5,4 @@ export const LOGO_URL =
 "https://s3.amazonaws.com/ionic-marketplace/food-ordering-restaurant-delivery-app/icon.png";
 
 export const MENU_URL = 
-"https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
+"https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Fmenu%2Fpl%3Fpage-type%3DREGULAR_MENU%26complete-menu%3Dtrue%26lat%3D12.9351929%26lng%3D77.62448069999999%26restaurantId%3D";
