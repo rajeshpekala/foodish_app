90b14e393bd0986d99f07c387e711d8735927096
rajeshpekala <pekalarajesh001@gmail.com>
2023-12-30 13:51:50 +0530

    connected_links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faFacebook, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { Link } from "react-router-dom";
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
 
@@ -15,16 +15,17 @@ className = "bg-black flex flex-row "
 
 <div  className = "flex flex-col px-8 py-1 mx-8 list-none text-white">
     <h3 className ="font-bold">Follow Us on</h3>
-    <li className="py-2 text-lg"><FontAwesomeIcon icon={faFacebook} size="2xl" style={{color: "#fefbfb",}} /></li>
-    <li className="py-2  text-lg"><FontAwesomeIcon icon={faInstagram} size="2xl" style={{color: "#fefbfb",}} /></li>
+    <li className="py-2 text-lg"><Link to = "https://www.facebook.com/"><FontAwesomeIcon icon={faFacebook} size="2xl" style={{color: "#fefbfb",}} /></Link></li>
+    <li className="py-2  text-lg"><Link to = "https://www.instagram.com/myself__p_r_e_m/"><FontAwesomeIcon icon={faInstagram} size="2xl" style={{color: "#fefbfb",}} /></Link></li>
     <li className ="py-2 text-white" > <FontAwesomeIcon icon={faPhone} size="xl" style={{color: "#fefbfb",}} /> 1800128283</li>
 </div>
 
 <div className = "flex flex-col px-8 py-1 list-none">
     <h3 className ="font-bold text-white ">Contact Us</h3>
-    <li className ="font-bold text-white text-sm ">Head Office</li>
+    <li className ="font-bold text-white text-sm pt-2">Head Office</li>
     <li className =" text-white">Srikakulam</li>
     <li className =" text-white">AndhraPradesh</li>
+    <li className="py-2  text-lg">< Link to = "https://www.linkedin.com/in/rajesh-pekala-a6827b259/"><FontAwesomeIcon icon={faLinkedin} size="2xl" style={{color: "#fefbfb",}} /></Link></li>
 </div> 
 
 <div className = "flex flex-col px-8 py-1 list-none">
