bd42192730e22a4225e3c0f9283c5958186c6fb4
rajeshpekala <pekalarajesh001@gmail.com>
2024-07-14 14:55:38 +0530

    added ShimmerUi

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,24 +1,16 @@
-const Shimmer = () => 
-    <div className = "shim_container">
-        <div className = "shimmer_card"></div>
-        <div className = "shimmer_card"></div>
-        <div className = "shimmer_card"></div>
-        <div className = "shimmer_card"></div>
-        <div className = "shimmer_card"></div>
-        <div className = "shimmer_card"></div>
-        <div className = "shimmer_card"></div>
-        <div className = "shimmer_card"></div>
-        <div className = "shimmer_card"></div>
-        <div className = "shimmer_card"></div>
-        <div className = "shimmer_card"></div>
-        <div className = "shimmer_card"></div>
-        <div className = "shimmer_card"></div>
-        <div className = "shimmer_card"></div>
-        <div className = "shimmer_card"></div>
-        <div className = "shimmer_card"></div>
-        <div className = "shimmer_card"></div>
-    </div>
+import React from "react";
 
-
+const Shimmer = () => (
+  <div className="flex flex-wrap justify-center">
+    {Array(18)
+      .fill("")
+      .map((_, index) => (
+        <div
+          key={index}
+          className="w-64 h-40 bg-gray-300 rounded-lg m-4 shimmer"
+        ></div>
+      ))}
+  </div>
+);
 
 export default Shimmer;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: [
-    "./src/**/*.{html,js}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
