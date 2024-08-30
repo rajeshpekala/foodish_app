c0b8e230b75b24ec44f009f6f71e8dc72389d902
rajeshpekala <pekalarajesh001@gmail.com>
2024-08-30 10:46:19 +0530

    Changed Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -28,38 +28,60 @@ const cartItems = useSelector((store) => store.cart.items);
 // ); 
 
     return (
-        <div className = "flex justify-between  bg-gradient-to-tr from-yellow-300 to-red-300 shadow-md m-">
-            <div className = "logo-container">
-            <Link to="/"><img className = "w-24 md:w-32 lg:w-24" src = {LOGO_URL} alt = "logo" /></Link>
-            
-            </div >
-          
-            <div className = "flex  items-center" >
-                <ul className = "flex p-3 m-3">
-                    <li className="px-4 font-bold text-lg hidden md:block">OnlineStatus:{
-                    onlinestatus?("✅"):("🚨") }</li>
-                    <li className="px-4"><Link to ="/"><FontAwesomeIcon icon={faHouse} size="2xl" /></Link></li>
-                    <li className="px-4 font-bold text-lg  "><Link to ="/about">About Us</Link></li>
-                    <li className="px-4 font-bold text-lg hidden md:block"><Link to = "/contact">Contact Us</Link></li>
-                    
-                    <li className="px-4 text-lg font-bold">
-                    <Link to = "/cart"><FontAwesomeIcon icon={faCartShopping} size="2xl" style={{color: "#183153",}} />
-                    ({cartItems.length}-Items)</Link>
-                        </li>
-                     
-                    <button className = "font-bold text-lg hidden md:block"
-                    onClick={ () => {
-                        btnname === "login"?
-                     setBtnname("logout"):
-                     setBtnname("login");   
-                    }}
-                    >{btnname}</button>
-                    <li className="px-4 py-1 font-bold text-lg hidden md:block">{loggedInUser}</li>
-                    <li></li>
-                </ul>
-                
-            </div>
+      <div className="flex justify-between  bg-gradient-to-tr from-yellow-300 to-red-300 shadow-md m-">
+        <div className="logo-container">
+          <Link to="/">
+            <img className="w-24 md:w-32 lg:w-24" src={LOGO_URL} alt="logo" />
+          </Link>
         </div>
+
+        <div className="flex  items-center">
+          <ul className="flex p-3 m-3">
+            <li className="px-4 font-bold text-lg hidden md:block">
+              OnlineStatus:{onlinestatus ? "✅" : "🚨"}
+            </li>
+            <li className="px-4">
+              <Link to="/">
+                <FontAwesomeIcon icon={faHouse} size="2xl" />
+              </Link>
+            </li>
+            <li className="px-4 font-bold text-lg  ">
+              <Link to="/about">About Us</Link>
+            </li>
+            <li className="px-4 font-bold text-lg hidden md:block">
+              <Link to="/contact">Contact Us</Link>
+            </li>
+            <li className="px-4 font-bold text-lg hidden md:block">
+              <Link to="/grocery">Grocery</Link>
+            </li>
+            <li className="px-4 text-lg font-bold">
+              <Link to="/cart">
+                <FontAwesomeIcon
+                  icon={faCartShopping}
+                  size="2xl"
+                  style={{ color: "#183153" }}
+                />
+                ({cartItems.length}-Items)
+              </Link>
+            </li>
+
+            <button
+              className="font-bold text-lg hidden md:block"
+              onClick={() => {
+                btnname === "login"
+                  ? setBtnname("logout")
+                  : setBtnname("login");
+              }}
+            >
+              {btnname}
+            </button>
+            <li className="px-4 py-1 font-bold text-lg hidden md:block">
+              {loggedInUser}
+            </li>
+            <li></li>
+          </ul>
+        </div>
+      </div>
     );
 };
 
